7eb043ee213ac788ec042bd2f153532fb8090e10
kssrao13882 <kssrao13882@gmail.com>
2022-04-30 10:36:58 +0530

    RMX1971: CorvusOS Bringup
    Signed-off-by: kssrao13882 <kssrao13882@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -5,4 +5,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/aosp_RMX1971.mk
+    $(LOCAL_DIR)/corvus_RMX1971.mk
+
+COMMON_LUNCH_CHOICES := \
+    corvus_RMX1971-userdebug \
+    corvus_RMX1971-user \
+    corvus_RMX1971-eng
